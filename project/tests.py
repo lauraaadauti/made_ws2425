c564b3e9f348a476c51f66a75fa604a4dad65457
Laura <dautila85913@th-nuernberg.de>
2024-12-11 12:55:12 +0100

    Project Work 6 - Continous Integration

--- project/tests.py
+++ project/tests.py
@@ -30,44 +30,6 @@ class TestPipeline(unittest.TestCase):
             self.assertIn("SecondarySchoolWomen", tables, "Table 'SecondarySchoolWomen' not found in SQLite database.")
             self.assertIn("TertiarySchoolWomen", tables, "Table 'TertiarySchoolWomen' not found in SQLite database.")
             print("Expected tables found in SQLite database.")    
-
-
-    def test_sqlite_table_content(self):
-        # test the content of the tables in the SQLite database.
-        print("Testing SQLite database table content...")
-        with sqlite3.connect(SQLITE_FILE) as conn:
-            parliamentWomen = pd.read_sql("SELECT * FROM parliamentWomen;", conn)
-            expectedYears = pd.read_sql("SELECT * FROM ExpectedYearsSchoolWomen;", conn)
-            secondarySchool = pd.read_sql("SELECT * FROM SecondarySchoolWomen;", conn)
-            tertiarySchool = pd.read_sql("SELECT * FROM TertiarySchoolWomen;", conn)
-            self.assertFalse(parliamentWomen.empty, "Table 'parliamentWomen' in SQLite database is empty.")
-            self.assertFalse(expectedYears.empty, "Table 'ExpectedYearsSchoolWomen' in SQLite database is empty.")
-            self.assertFalse(secondarySchool.empty, "Table 'SecondarySchoolWomen' in SQLite database is empty.")
-            self.assertFalse(tertiarySchool.empty, "Table 'TertiarySchoolWomen' in SQLite database is empty.")
-            print("SQLite database tables contain valid data.")
-            
-            # test for the correct columns in the tables
-            print("Testing the columns of the tables...")
-            expected_columns_parliamentWomen = ['Country Name', 'Year', 'Value']  
-            expected_columns_expectedYears = ['Country Name', 'Year', 'Value']   
-            expected_columns_secondarySchool = ['Country Name', 'Year', 'Value']  
-            expected_columns_tertiarySchool = ['Country Name', 'Year', 'Value']  
-            
-            self.assertTrue(set(expected_columns_parliamentWomen).issubset(parliamentWomen.columns), "Missing columns in 'parliamentWomen'.")
-            self.assertTrue(set(expected_columns_expectedYears).issubset(expectedYears.columns), "Missing columns in 'ExpectedYearsSchoolWomen'.")
-            self.assertTrue(set(expected_columns_secondarySchool).issubset(secondarySchool.columns), "Missing columns in 'SecondarySchoolWomen'.")
-            self.assertTrue(set(expected_columns_tertiarySchool).issubset(tertiarySchool.columns), "Missing columns in 'TertiarySchoolWomen'.")
-            print("SQLite tables contain the correct columns.")
-            
-            # test for a minimal number of rows, minimum 1 row for each country should be present, so 4 rows minimum for each table
-            print("Testing the rows of the tables...")
-            self.assertGreater(len(parliamentWomen), 4, "Table 'parliamentWomen' has no rows.")
-            self.assertGreater(len(expectedYears), 4, "Table 'ExpectedYearsSchoolWomen' has no rows.")
-            self.assertGreater(len(secondarySchool), 4, "Table 'SecondarySchoolWomen' has no rows.")
-            self.assertGreater(len(tertiarySchool), 4, "Table 'TertiarySchoolWomen' has no rows.")
-            
-            print("SQLite tables contain valid rows.")
-    
     
     def test_sqlite_table_not_empty(self):
         # test the content of the tables in the SQLite database.
